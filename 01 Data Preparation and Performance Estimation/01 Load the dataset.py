c4e6613add57ea6fd26b1f97b052baa782210892
Sabin <spradh@users.noreply.github.com>
2024-03-06 22:11:57 -0600

    Update 01 Load the dataset.py

--- 01 Data Preparation and Performance Estimation/01 Load the dataset.py
+++ 01 Data Preparation and Performance Estimation/01 Load the dataset.py
@@ -2,10 +2,10 @@
 import nannyml
 
 # Load US Census Employment dataset
-reference, anaylsis, anaylsis_gt = nannyml.load_us_census_ma_employment_data()
+reference, analysis, analysis_gt = nannyml.load_us_census_ma_employment_data()
 
 # Print head of the reference data
 print(reference.head())
 
 # Print head of the analysis data
-print(anaylsis.head())
+print(analysis.head())
