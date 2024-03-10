424d22469392772e3c6e088a35de470ad28e1539
Sabin <spradh@users.noreply.github.com>
2024-03-10 13:00:53 -0500

    Create 04 Interacting with results.py

---
+++ 02 Monitoring Performance and Business Value/04 Interacting with results.py
@@ -0,0 +1,11 @@
+# Filter estimated results for the roc_auc metric and convert them to a dataframe
+display(estimated_results.filter(metrics=['roc_auc']).to_df())
+
+# Filter estimated results for the reference period and convert them to a dataframe
+display(estimated_results.filter(period='analysis').to_df())
+
+# Filter the estimated results for the accuracy metric
+display(estimated_results.filter(metrics=['accuracy']).plot().show())
+
+# Filter the estimated results for the analysis period, as well as for accuracy and roc_auc metrics
+display(estimated_results.filter(period='analysis', metrics=['accuracy', 'roc_auc']).plot().show())
